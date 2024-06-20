e857148ef9597efd617e1a69117d4a529eee1d03
Cuong Chi Tran <tranc2@carleton.edu>
2024-06-20 13:55:03 -0500

    Minor refinements

--- semantic_search/mainllama.py
+++ semantic_search/mainllama.py
@@ -23,29 +23,29 @@ SOURCE: https://platform.openai.com/docs/api-reference/introduction?lang=python
 """
 
 
-import openai, os, tiktoken
-from openai import OpenAI
-from pgpt_python.client import PrivateGPTApi # type: ignore
+# import tiktoken
+import os, pgpt_python
+# from pgpt_python.client import PrivateGPTApi
 
-def num_tokens_from_messages(messages, model="TheBloke/Llama-2-13B-chat-GGML"):
-  """Returns the number of tokens used by a list of messages."""
-  try:
-      encoding = tiktoken.encoding_for_model(model)
-  except KeyError:
-      encoding = tiktoken.get_encoding("cl100k_base")
-  if model == "":  # note: future models may deviate from this
-      num_tokens = 0
-      for message in messages:
-          num_tokens += 4  # every message follows <im_start>{role/name}\n{content}<im_end>\n
-          for key, value in message.items():
-              num_tokens += len(encoding.encode(value))
-              if key == "name":  # if there's a name, the role is omitted
-                  num_tokens += -1  # role is always required and always 1 token
-      num_tokens += 2  # every reply is primed with <im_start>assistant
-      return num_tokens
-  else:
-      raise NotImplementedError(f"""num_tokens_from_messages() is not presently implemented for model {model}.
-  See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens.""")
+# def num_tokens_from_messages(messages, model="TheBloke/Llama-2-13B-chat-GGML"):
+#   """Returns the number of tokens used by a list of messages."""
+#   try:
+#       encoding = tiktoken.encoding_for_model(model)
+#   except KeyError:
+#       encoding = tiktoken.get_encoding("cl100k_base")
+#   if model == "":  # note: future models may deviate from this
+#       num_tokens = 0
+#       for message in messages:
+#           num_tokens += 4  # every message follows <im_start>{role/name}\n{content}<im_end>\n
+#           for key, value in message.items():
+#               num_tokens += len(encoding.encode(value))
+#               if key == "name":  # if there's a name, the role is omitted
+#                   num_tokens += -1  # role is always required and always 1 token
+#       num_tokens += 2  # every reply is primed with <im_start>assistant
+#       return num_tokens
+#   else:
+#       raise NotImplementedError(f"""num_tokens_from_messages() is not presently implemented for model {model}.
+#   See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens.""")
 
 
 try: # For ai_chat_bot.py access
